542a5a82e5080661fc5482943226ae813b514f77
name <1119313277@qq.com>
2023-03-26 16:33:21 +0800

    通过引导ChatGPT完成代码

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>TodoList</h1>
+    <form @submit.prevent="addTodo">
+      <input type="text" v-model="newTodo" placeholder="Add New Todo">
+      <button>Add Todo</button>
+    </form>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <input type="checkbox" v-model="todo.completed">
+        <span :class="{ completed: todo.completed }">{{ todo.text }}</span>
+        <span v-if="todo.created" class="created-time">{{ todo.created.toLocaleString() }}</span>
+        <button @click="removeTodo(index)">Delete</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const newTodo = ref('');
+    const todos = ref([
+      { text: 'Learn Vue', completed: false },
+      { text: 'Build a Vue app', completed: false },
+    ]);
+
+    function addTodo() {
+      const newTodoObj = {
+        text: newTodo.value,
+        completed: false,
+        created: new Date(),
+      };
+      if (newTodoObj.text === '') {
+        return;
+      }
+      todos.value.push(newTodoObj);
+      newTodo.value = '';
+    }
+
+    function removeTodo(index) {
+      todos.value.splice(index, 1);
+    }
+
+    return {
+      newTodo,
+      todos,
+      addTodo,
+      removeTodo,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.completed {
+  text-decoration: line-through;
+}
+
+.created-time {
+  font-size: 12px;
+  margin-left: 10px;
+  color: gray;
+}
+
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
